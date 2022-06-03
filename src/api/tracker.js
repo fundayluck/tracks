40057a662592344daf1e3afa349fdb8a3758fa13
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-04 06:43:37 +0700

    making an API request

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://04f1-2001-448a-2011-f3c-b445-5705-5b5b-f14c.ngrok.io",
+  baseURL: "https://0535-2001-448a-2011-f3c-b445-5705-5b5b-f14c.ap.ngrok.io",
 });
