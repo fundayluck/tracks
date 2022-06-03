d548e22bd124c23aaa8b2cb48f0521d512f4a093
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-04 04:57:27 +0700

    adding signin, signout and signup in authcontext js file

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,20 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {};
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {};
+};
+
+const signout = (dispatch) => {
+  return () => {};
+};
+
 export const { Provider, Context } = CreateDataContext(
   authReducer,
-  {},
+  { signin, signup, signout },
   { isSignedIn: false }
 );
