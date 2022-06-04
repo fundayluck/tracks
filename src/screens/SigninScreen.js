b2c4e448732c6548f9231ba062a428f3a24c46fb
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-04 22:19:08 +0700

    creating sign in

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,39 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
+import { View, StyleSheet } from "react-native";
 
 const SigninScreen = () => {
+  const { state, Signin } = useContext(AuthContext);
   return (
-    <View style={{ marginTop: 50 }}>
-      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage=""
+        submitButtonText="Sign In"
+        onSubmit="Signin"
+      />
+      <NavLink
+        text="Don't have an account? go back to Sign up!"
+        routeName="Signup"
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default SigninScreen;
