52213177e6373a25cee8ce3574b3724d608da590
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-04 23:55:25 +0700

    update ngrok

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://53af-2001-448a-2011-f3c-b8ca-c86b-af22-bd78.ap.ngrok.io",
+  baseURL: "https://6a9e-110-138-84-204.ap.ngrok.io",
 });
