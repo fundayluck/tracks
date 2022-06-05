abff4a62e15e01ab9abd0917de48dd5d3032ce10
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-05 22:56:40 +0700

    showing a map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 const Map = () => {
-  return <Text>Map Location in here</Text>;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: -6.28542872003845,
+        longitude: 107.04080237435055,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
 
 export default Map;
