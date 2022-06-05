86e58ff4fea0ee1dc2f7db735d67c40afb3594ee
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-05 23:05:27 +0700

    drawing a series of points

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: -6.28542872003845 + i * 0.001,
+        longitude: 107.04080237435055 + i * 0.001,
+      });
+    } else {
+      points.push({
+        latitude: -6.28542872003845 - i * 0.002,
+        longitude: 107.04080237435055 + i * 0.001,
+      });
+    }
+  }
   return (
     <MapView
       style={styles.map}
@@ -12,7 +26,9 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    />
+    >
+      <Polyline coordinates={points} />
+    </MapView>
   );
 };
 
