c592f34088f80ec36b40d4a5133aa2f0cf5b9c7f
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-05 23:46:45 +0700

    requesting location permissions

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Button } from "react-native-elements";
+import { Button, Text } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
 
@@ -9,7 +9,9 @@ const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
   return (
     <SafeAreaView>
-      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Text style={{ alignSelf: "center" }} h2>
+        AccountScreen
+      </Text>
       <Spacer>
         <Button title="Sign Out" onPress={signout} />
       </Spacer>
