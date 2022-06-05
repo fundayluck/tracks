08382b26a13f1792a2e40f6781a3168545d7c53a
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-05 22:28:15 +0700

    create a map component

---
+++ src/components/Map.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const Map = () => {
+  return <Text>Map Location in here</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
