8999c4dcb3839f68e58f2db2622774564c056dd0
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-06 13:36:52 +0700

    Location Context

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import TrackListScreen from "./src/screens/TrackListScreen";
 import { Provider as AuthProvider } from "./src/context/AuthContext";
 import { setNavigator } from "./src/navigationRef";
 import AuthScreen from "./src/screens/ResolveAuthScreen";
+import { Provider as LocationProvider } from "./src/context/LocationContext";
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuth: AuthScreen,
@@ -32,12 +33,14 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <AuthProvider>
-      <App
-        ref={(navigator) => {
-          setNavigator(navigator);
-        }}
-      />
-    </AuthProvider>
+    <LocationProvider>
+      <AuthProvider>
+        <App
+          ref={(navigator) => {
+            setNavigator(navigator);
+          }}
+        />
+      </AuthProvider>
+    </LocationProvider>
   );
 };
--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = (increment) => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees,
+      longitude: 107.04080237435055 + increment * tenMetersWithDegrees,
+      latitude: -6.28542872003845 + increment * tenMetersWithDegrees,
     },
   };
 };
