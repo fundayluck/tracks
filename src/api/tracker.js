4b085229d6b2d3e5972923a9f415411e1424aec4
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-07 08:46:53 +0700

    live location updates

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://1ba8-2001-448a-2011-f3c-e9e7-d6a6-d403-2188.ap.ngrok.io",
+  baseURL: "https://e2cf-2001-448a-2011-f3c-7cff-bb0c-e44a-8537.ap.ngrok.io",
 });
