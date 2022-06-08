0a653879cbcf73b134113079b9a9a24710072c9d
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-08 08:58:13 +0700

    fix bug

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -3,16 +3,13 @@ import createDataContext from "./CreateDataContext";
 const locationReducer = (state, action) => {
   switch (action.type) {
     case "add_current_location":
-      return {
-        ...state,
-        currentLocation: action.payload,
-      };
+      return { ...state, currentLocation: action.payload };
     case "start_recording":
       return { ...state, recording: true };
     case "stop_recording":
       return { ...state, recording: false };
     case "add_location":
-      return { ...state, location: [...state.locations, action.payload] };
+      return { ...state, locations: [...state.locations, action.payload] };
     case "change_name":
       return { ...state, name: action.payload };
     default:
@@ -23,15 +20,12 @@ const locationReducer = (state, action) => {
 const changeName = (dispatch) => (name) => {
   dispatch({ type: "change_name", payload: name });
 };
-
 const startRecording = (dispatch) => () => {
   dispatch({ type: "start_recording" });
 };
-
 const stopRecording = (dispatch) => () => {
   dispatch({ type: "stop_recording" });
 };
-
 const addLocation = (dispatch) => (location, recording) => {
   dispatch({ type: "add_current_location", payload: location });
   if (recording) {
@@ -41,11 +35,6 @@ const addLocation = (dispatch) => (location, recording) => {
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  {
-    startRecording,
-    stopRecording,
-    addLocation,
-    changeName,
-  },
+  { startRecording, stopRecording, addLocation, changeName },
   { name: "", recording: false, locations: [], currentLocation: null }
 );
